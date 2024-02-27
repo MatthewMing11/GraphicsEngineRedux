3d130dc5eac449a44dd1bcbe60691ea09bfaf25e
MatthewMing11 <matthew.ming11@myhunter.cuny.edu>
2024-02-27 01:38:24 -0500

    added midpoint circle algo

--- raster.cpp
+++ raster.cpp
@@ -21,7 +21,7 @@ uint32_t *textureBuffer;
 void drawLineLow(Point &p1, Point &p2){
     int dx=p2.x-p1.x;
     int dy=p2.y-p1.y;
-    int prevy=p1.y;
+    int curry=p1.y;
     int yinc=1;
     if(dy<0){
         dy=-dy;
@@ -29,9 +29,9 @@ void drawLineLow(Point &p1, Point &p2){
     }
     int D=2*dy-dx;
     for(int x=p1.x;x<p2.x;x++){
-        textureBuffer[(prevy*width) + x] = 0xFFFFFFFF;
+        textureBuffer[(curry*width) + x] = 0xFFFFFFFF;
         if(D>0){
-            prevy+=yinc;
+            curry+=yinc;
             D=D-2*dx;
         }
         D=D+2*dy;
@@ -41,7 +41,7 @@ void drawLineLow(Point &p1, Point &p2){
 void drawLineHigh(Point &p1, Point &p2){
     int dx=p2.x-p1.x;
     int dy=p2.y-p1.y;
-    int prevx=p1.x;
+    int currx=p1.x;
     int xinc=1;
     if(dx<0){
         dx=-dx;
@@ -49,9 +49,9 @@ void drawLineHigh(Point &p1, Point &p2){
     }
     int D=2*dx-dy;
     for(int y=p1.y;y<p2.y;y++){
-        textureBuffer[(y*width) + prevx] = 0xFFFFFFFF;
+        textureBuffer[(y*width) + currx] = 0xFFFFFFFF;
         if(D>0){
-            prevx+=xinc;
+            currx+=xinc;
             D=D-2*dy;
         }
         D=D+2*dx;
@@ -73,10 +73,34 @@ void drawLine(Point &p1, Point &p2){
         }
     }
 }
-// void drawCircle(Point p1, int diam){
-
-
-// }
+void drawCircle(Point p1, int radius){
+    int fast_coord=0;//the addend for the coordinate that always increments
+    int slow_coord=radius;//the addend for the coordinate that sometimes increments
+    int E=3-2*radius;// E is the change in the decision formula used to determine whether or not to decrement the slow coord
+    while(true){
+        std::cout<<(p1.x+slow_coord)<<","<<(p1.y+fast_coord)<<std::endl;
+        std::cout<<(p1.x+fast_coord)<<","<<(p1.y+slow_coord)<<std::endl;
+        textureBuffer[((p1.y+fast_coord)*width) + (p1.x+slow_coord)] = 0xFFFFFFFF;// Octant 1
+        textureBuffer[((p1.y+slow_coord)*width) + (p1.x+fast_coord)] = 0xFFFFFFFF;// Octant 2
+        textureBuffer[((p1.y+slow_coord)*width) + (p1.x-fast_coord)] = 0xFFFFFFFF;// Octant 3
+        textureBuffer[((p1.y+fast_coord)*width) + (p1.x-slow_coord)] = 0xFFFFFFFF;// Octant 4
+        textureBuffer[((p1.y-fast_coord)*width) + (p1.x-slow_coord)] = 0xFFFFFFFF;// Octant 5
+        textureBuffer[((p1.y-slow_coord)*width) + (p1.x-fast_coord)] = 0xFFFFFFFF;// Octant 6
+        textureBuffer[((p1.y-slow_coord)*width) + (p1.x+fast_coord)] = 0xFFFFFFFF;// Octant 7
+        textureBuffer[((p1.y-fast_coord)*width) + (p1.x+slow_coord)] = 0xFFFFFFFF;// Octant 8
+        // Generates pixels based on octant 1
+        if(E>0){
+            E=E+2*(5-2*slow_coord+2*fast_coord);
+            slow_coord-=1;
+        }else{
+            E=E+2*(3+2*fast_coord);
+        }
+        fast_coord+=1;
+        if(fast_coord>slow_coord){
+            break;
+        }
+    }
+}
 int main(int argc, char* argv[]){
     // Dimensions of screen
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -91,6 +115,10 @@ int main(int argc, char* argv[]){
     p2.x=220;
     p2.y=100;
     drawLine(p1,p2);
+    Point p3;
+    p3.x=250;
+    p3.y=250;
+    drawCircle(p3,100);
     SDL_UpdateTexture( texture , NULL, textureBuffer, width * sizeof (uint32_t));
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer,texture, NULL,NULL);
