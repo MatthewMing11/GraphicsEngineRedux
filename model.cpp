86e820656d64a6812d0768b08c28cda268e44602
MatthewMing11 <matthew.ming11@myhunter.cuny.edu>
2024-04-18 03:33:51 -0400

    moved vector normal code from function to setup code

--- model.cpp
+++ model.cpp
@@ -35,9 +35,13 @@ Model::Model(char* path)
             zMax=std::max(zMax,data.z);
             vertices.push_back(data);
         }
-        if (tokens[0]=="f"){
+        else if (tokens[0]=="f"){
             struct Face data={{std::stoi(tokens[1]),std::stoi(tokens[2]),std::stoi(tokens[3])}};
             faces.push_back(data);
+        }
+        else if(tokens[0]=="vn"){
+            Normal data = {std::stof(tokens[1]),std::stof(tokens[2]),std::stof(tokens[3])};
+            normals.push_back(data);
         }
     }
     this.faces=faces;
@@ -46,18 +50,36 @@ Model::Model(char* path)
     scaleY=(yMax-yMin)/height;
     this.xMin=xMin;
     this.yMin=yMin;
+    if (normals.size()==0){
+        for(int i=0;i<vertices.size();i++){
+            Normal n = {0.f,0.f,0.f};
+            normals.push_back(n);
+        }
+        for(int i=0;i<faces;i++){
+            Vec3f A = vertices[faces[i]-1];
+            Vec3f B = vertices[faces[i+1]-1];
+            Vec3f C = vertices[faces[i+2]-1];
+            Vec3f AB=B-A;
+            Vec3f AC=C-A;
+            Vec3f ABxAC=AB^AC;
+            normals[faces[i]-1]+=ABxAC;
+            normals[faces[i+1]-1]+=ABxAC;
+            normals[faces[i+2]-1]+=ABxAC;
+
+        }
+        for(int i=0;i<normals.size();i++){
+            normals[i]=normals[i].normalize();
+        }
+    }
 }
 Vec3f v_normal(int iface, int nthvert){
-    Vec3f vec1=Vec3f(vertices[face.third-1].x-vertices[face.first-1].x,vertices[face.third-1].y-vertices[face.first-1].y,vertices[face.third-1].z-vertices[face.first-1].z);
-    Vec3f vec2=Vec3f(vertices[face.second-1].x-vertices[face.first-1].x,vertices[face.second-1].y-vertices[face.first-1].y,vertices[face.second-1].z-vertices[face.first-1].z);
-    Vec3f v_normal = vec1^vec2;
-    v_normal.normalize();
+    Vec3f v_normal = normals[faces[iface].points[nthvert]-1];
     return v_normal;
 }
 Matrix vert(int iface, int nthvert){
     Matrix res = new Matrix(1,4);
     res[0][0]=static_cast<int>((vertices[faces[iface].points[nthvert]-1].x-xMin)/scaleX);
-    res[0][1]=static_cast<int>((vertices[faces[iface].points[nthvert]-1]-yMin)/scaleY);
+    res[0][1]=static_cast<int>((vertices[faces[iface].points[nthvert]-1].y-yMin)/scaleY);
     res[0][2]=0;
     res[0][3]=1;
     return res;
