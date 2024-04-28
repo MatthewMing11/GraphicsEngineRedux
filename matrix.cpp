ffc5d4306f98df6aacafc492b82b76c547c93af1
MatthewMing11 <matthew.ming11@myhunter.cuny.edu>
2024-04-28 00:32:31 -0400

    fixed matrix copy constructor and assignment operator and reverted debugging code to original

--- matrix.cpp
+++ matrix.cpp
@@ -1,7 +1,7 @@
 #include "matrix.h"
 #include "vec3f.h"
 #include <iostream>
-
+#include <cassert>
 Matrix::Matrix(int row,int col){
     arr = new float*[row];
     for(int i=0;i<row;++i){
@@ -26,9 +26,23 @@ Matrix::Matrix(const Matrix& other): w(other.w),h(other.h){
     for(int i=0;i<h;++i){
         arr[i]= new float[w];
     }
-    operator=(other);
+    for(int i=0;i<h;++i){
+        for(int j=0;j<w;++j){
+            arr[i][j]= other.arr[i][j];
+        }
+    }
 }
 Matrix& Matrix::operator=(const Matrix& other){
+    for(int i=0;i<h;i++){
+        delete[] arr[i];
+    }
+    delete[] arr;
+    h=other.h;
+    w=other.w;
+    arr = new float*[h];
+    for(int i=0;i<h;++i){
+        arr[i]= new float[w];
+    }
     for(int i=0;i<h;++i){
         for(int j=0;j<w;++j){
             arr[i][j]= other.arr[i][j];
@@ -52,21 +66,10 @@ const float& Matrix::operator()(int x, int y) const {
 } 
 Matrix Matrix::operator*(Matrix const& obj)//matrix multiplication
 {
-    //gotta fix something breaking in here
-    // std::cout<<"Matrix function"<<std::endl;
-    // std::cout<<w<<std::endl;
-    // std::cout<<h<<std::endl;
-    // std::cout<<obj.w<<std::endl;
-    // std::cout<<obj.h<<std::endl;
-    // std::cout<<obj.arr<<std::endl;
     Matrix res=Matrix(h,obj.w);
     for (int i=0;i<h;i++){
         for(int j=0;j<obj.w;j++){
             for(int k=0;k<obj.h;k++){
-                // std::cout<<i<<","<<j<<","<<k<<std::endl;
-                // std::cout<<obj.arr[k][j]<<std::endl;
-                // std::cout<<arr[i][k]<<std::endl;
-                // std::cout<<obj.arr[k][j]<<std::endl;
                 res(i,j)+=arr[i][k]*obj.arr[k][j];
             }
         }
